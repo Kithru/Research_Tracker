8594605dade5bf462c6ce5423b5d2e16972a1e01
Kithru Viduranga <kithruV@icbtcampus.local>
2025-11-01 20:23:33 +0530

    Initial commit - Create a spring boot package

---
+++ research-tracker/src/test/java/lk/ijse/cmjd/researchtracker/research_tracker/ResearchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd.researchtracker.research_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
