0d183df55eac7ee19c87f05f4c386ad5671458ac
Kithru Viduranga <kithruV@icbtcampus.local>
2025-11-04 23:58:26 +0530

    Update the package strcuture

--- research-tracker/src/main/java/lk/ijse/cmjd/researchtracker/ResearchTrackerApplication.java
+++ research-tracker/src/main/java/lk/ijse/cmjd/researchtracker/ResearchTrackerApplication.java
@@ -1,4 +1,4 @@
-package lk.ijse.cmjd.researchtracker.research_tracker;
+package lk.ijse.cmjd.researchtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,6 @@ public class ResearchTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResearchTrackerApplication.class, args);
-		
 	}
 
 }
