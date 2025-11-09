98d420584b1ec2fa0012717c40bebc9b25d62f70
Kithru Viduranga <kithruV@icbtcampus.local>
2025-11-09 13:33:18 +0530

    Add status and user role models

---
+++ research-tracker/src/main/java/lk/ijse/cmjd/model/Status.java
@@ -0,0 +1,9 @@
+package lk.ijse.cmjd.model;
+
+public enum Status {
+    PLANNING,
+    ACTIVE,
+    ON_HOLD,
+    COMPLETED,
+    ARCHIVED
+}
--- research-tracker/src/main/java/lk/ijse/cmjd/researchtracker/ResearchTrackerApplication.java
+++ research-tracker/src/main/java/lk/ijse/cmjd/researchtracker/ResearchTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResearchTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResearchTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchTrackerApplication.class, args);
+    }
 
 }
